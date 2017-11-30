331049c9d63413091a5798817b84cfcd7d51172f
Lukas Kaplan <lukas.kaplan@lkaplan.cz>
2017-11-30 12:43:52 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+HELLO
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+setInterval(function(){
+$("#screen").load('banners.php')
+}, 2000);
+});
+</script>
+
+<div id="screen"></div>
